a9913b971362d79216b2c1290422f8fbd83341f5
jero <omineroj@gmail.com>
2025-10-13 13:15:46 -0500

    Update test SaveUserUseCaseTest

--- src/test/java/accrox/aros/api/user/SaveUserUseCaseTest.java
+++ src/test/java/accrox/aros/api/user/SaveUserUseCaseTest.java
@@ -1,9 +1,11 @@
 package accrox.aros.api.user;
 
+import accrox.aros.api.application.dto.auth.User.CreateUserInput;
 import accrox.aros.api.application.dto.auth.User.CreateUserOutput;
 import accrox.aros.api.application.usecases.auth.User.SaveUserUseCase;
 import accrox.aros.api.domain.model.User;
 import accrox.aros.api.domain.repository.UserRepository;
+import accrox.aros.api.infrastructure.spring.controllers.auth.dto.CreateUserRequest;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
@@ -26,16 +28,25 @@ public class SaveUserUseCaseTest {
     private SaveUserUseCase saveUserUseCase;
 
     // Saved user
-   /* @Test
+    @Test
     void whenValidDTO_thenSavesUserSuccessfully() {
 
-        CreateUserOutput dto = new CreateUserOutput(
+        CreateUserRequest request = new CreateUserRequest(
                 "Carlos Pérez",
                 "12345678",
                 "carlos@example.com",
                 "StrongPassword1",
                 "Calle Falsa 123",
                 "5551234567"
+        );
+
+        CreateUserInput dto = new CreateUserInput(
+                request.name(),
+                request.document(),
+                request.email(),
+                request.password(),
+                request.address(),
+                request.phone()
         );
 
         when(userRepository.findByDocument(dto.document())).thenReturn(Optional.empty());
@@ -66,6 +77,6 @@ public class SaveUserUseCaseTest {
         assertEquals(dto.phone(), savedUser.getPhone());
 
         assertDoesNotThrow(() -> saveUserUseCase.execute(dto));
-    }*/
+    }
 
 }
