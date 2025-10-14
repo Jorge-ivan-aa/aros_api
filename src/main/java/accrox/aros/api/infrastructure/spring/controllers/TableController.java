893411f8e115fd385188a161ed367051d31a06dc
EddSanchezz <edwarde.sanchezm@uqvirtual.edu.co>
2025-10-14 14:46:24 -0500

    Fix: Security adaptation

--- src/main/java/accrox/aros/api/infrastructure/spring/controllers/TableController.java
+++ src/main/java/accrox/aros/api/infrastructure/spring/controllers/TableController.java
@@ -3,20 +3,25 @@ package accrox.aros.api.infrastructure.spring.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import accrox.aros.api.application.dto.Table.CreateTableDto;
 import accrox.aros.api.application.usecases.Table.CreateTableUseCase;
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import jakarta.validation.Valid;
 
 @RestController
+@RequestMapping(path = "/tables")
 public class TableController {
     
     @Autowired
     private CreateTableUseCase createTableUseCase;
 
+    @PostMapping("/create")
     public ResponseEntity<?> create(
-        @RequestBody CreateTableDto request
+        @Valid @RequestBody CreateTableDto request
     ) {
         this.createTableUseCase.execute(request);
         return new ResponseEntity<>(HttpStatus.CREATED);
