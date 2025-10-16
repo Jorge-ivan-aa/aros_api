851dde7bbe264bfcbababbe017b58538294fa9d5
Jorge-ivan-aa <jorgei.acostaa@uqvirtual.edu.co>
2025-10-16 11:22:04 -0500

    chore: update necesary configs

--- src/main/java/accrox/aros/api/infrastructure/spring/config/beans/ProductBeansConfig.java
+++ src/main/java/accrox/aros/api/infrastructure/spring/config/beans/ProductBeansConfig.java
@@ -4,14 +4,19 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import accrox.aros.api.application.usecases.product.CreateProductUseCase;
+import accrox.aros.api.application.usecases.product.UpdateProductUseCase;
 import accrox.aros.api.domain.repository.ProductRepository;
 
 @Configuration
 public class ProductBeansConfig {
     @Bean
     public CreateProductUseCase createProductUseCase(
-        ProductRepository repository
-    ) {
+            ProductRepository repository) {
         return new CreateProductUseCase(repository);
+    }
+
+    @Bean
+    public UpdateProductUseCase updateProductUseCase(ProductRepository repository) {
+        return new UpdateProductUseCase(repository);
     }
 }
