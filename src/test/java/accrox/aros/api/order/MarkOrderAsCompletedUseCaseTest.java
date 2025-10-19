10bbfd776f1c814e272cd59dc84248ea70d20afc
jero <omineroj@gmail.com>
2025-10-19 16:08:22 -0500

    MarkOrderAsComplete Use case test

---
+++ src/test/java/accrox/aros/api/order/MarkOrderAsCompletedUseCaseTest.java
@@ -0,0 +1,36 @@
+package accrox.aros.api.order;
+
+import accrox.aros.api.application.usecases.order.MarkOrderAsCompletedUseCase;
+import accrox.aros.api.domain.repository.OrderRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.mockito.Mockito.*;
+
+
+class MarkOrderAsCompletedUseCaseTest {
+
+    @Mock
+    private OrderRepository orderRepository;
+
+    @InjectMocks
+    private MarkOrderAsCompletedUseCase markOrderAsCompletedUseCase;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void when_executeWithValidOrderId_then_markOrderAsCompletedCalled() {
+        Long orderId = 1L;
+
+        markOrderAsCompletedUseCase.execute(orderId);
+
+        verify(orderRepository, times(1)).MarkOrderAsCompleted(orderId);
+    }
+
+}
